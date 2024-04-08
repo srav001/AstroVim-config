70492585e8d65940035675f483a9f193ca3a66df
Sravan <sravansuresh1x@gmail.com>
2024-04-08 12:37:43 +0530

    fix: minor

--- lua/community.lua
+++ lua/community.lua
@@ -5,6 +5,6 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity/colorscheme/catppuccin" },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,7 +7,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
         "tsserver",
 
         "eslint",
@@ -31,7 +31,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "prettier",
-        -- "stylua",
+        "stylua",
         -- add more arguments for adding more null-ls sources
       })
     end,
