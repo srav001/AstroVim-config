6f7d40e4f9eddadc9f39ee72cd7ac5bf70a129a4
Sravan <sravansuresh1x@gmail.com>
2024-07-24 00:14:09 +0530

    feat: updates

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,12 @@
 ---@type LazySpec
 return {
   {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+  {
     "folke/trouble.nvim",
     cmd = { "Trouble", "TroubleToggle" },
     lazy = true,
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,21 +1,25 @@
 ---@type LazySpec
 return {
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   event = { "InsertEnter" },
+  --   cmd = { "Copilot" },
+  --   opts = {
+  --     suggestion = {
+  --       enabled = true,
+  --       auto_trigger = true,
+  --       debounce = 75,
+  --       keymap = {
+  --         accept = "<C-l>",
+  --         accept_word = false,
+  --         accept_line = false,
+  --         dismiss = "<C-]>",
+  --       },
+  --     },
+  --   },
+  -- },
   {
-    "zbirenbaum/copilot.lua",
-    event = { "InsertEnter" },
-    cmd = { "Copilot" },
-    opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          dismiss = "<C-]>",
-        },
-      },
-    },
+    "supermaven-inc/supermaven-nvim",
+    config = function() require("supermaven-nvim").setup {} end,
   },
 }
